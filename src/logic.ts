21dfad98ea8e096b570430c7d3e77cda1db05fec
Xavier Raffin <xavier.raffin@visualping.io>
2021-10-26 21:24:38 -0700

    Initial implem

--- src/logic.ts
+++ src/logic.ts
@@ -1,68 +1,229 @@
-import { InfoResponse, GameState, MoveResponse, Game } from "./types"
+import { InfoResponse, GameState, MoveResponse, Game } from "./types";
 
 export function info(): InfoResponse {
-    console.log("INFO")
-    const response: InfoResponse = {
-        apiversion: "1",
-        author: "",
-        color: "#888888",
-        head: "default",
-        tail: "default"
-    }
-    return response
+  console.log("INFO");
+  const response: InfoResponse = {
+    apiversion: "1",
+    author: "Gabriel",
+    color: "#00FF00",
+    head: "pixel",
+    tail: "pixel",
+  };
+  return response;
 }
 
 export function start(gameState: GameState): void {
-    console.log(`${gameState.game.id} START`)
+  console.log(`${gameState.game.id} START`);
 }
 
 export function end(gameState: GameState): void {
-    console.log(`${gameState.game.id} END\n`)
+  console.log(`${gameState.game.id} END\n`);
+}
+
+function avoidSnakeBody(body: any[], myhead: any, possibleMoves: any): void {
+  // We can ignore the tail as he will move away
+  for (let i = 0; i < body.length - 1; i++) {
+    if (body[i].x == myhead.x) {
+      if (body[i].y == myhead.y + 1) {
+        trace("There is a body on the up");
+        possibleMoves.up = false;
+      } else if (body[i].y == myhead.y - 1) {
+        trace("There is a body on the down");
+        possibleMoves.down = false;
+      }
+    }
+    if (body[i].y == myhead.y) {
+      if (body[i].x == myhead.x + 1) {
+        trace("There is a body on the right");
+        possibleMoves.right = false;
+      } else if (body[i].x == myhead.x - 1) {
+        trace("There is a body on the left");
+        possibleMoves.left = false;
+      }
+    }
+  }
+}
+
+function isOnThisSide(snakeHead: any, myhead: any, direction: string): boolean {
+  switch (direction) {
+    case "up":
+      if (
+        (snakeHead.x == myhead.x && snakeHead.y - 2 == myhead.y) ||
+        (snakeHead.x == myhead.x - 1 && snakeHead.y - 1 == myhead.y) ||
+        (snakeHead.x == myhead.x + 1 && snakeHead.y - 1 == myhead.y)
+      ) {
+        return true;
+      }
+    case "down":
+      if (
+        (snakeHead.x == myhead.x && snakeHead.y + 2 == myhead.y) ||
+        (snakeHead.x == myhead.x - 1 && snakeHead.y + 1 == myhead.y) ||
+        (snakeHead.x == myhead.x + 1 && snakeHead.y + 1 == myhead.y)
+      ) {
+        return true;
+      }
+    case "right":
+      if (
+        (snakeHead.y == myhead.y && snakeHead.x - 2 == myhead.x) ||
+        (snakeHead.y == myhead.y - 1 && snakeHead.x - 1 == myhead.x) ||
+        (snakeHead.y == myhead.y + 1 && snakeHead.x - 1 == myhead.x)
+      ) {
+        return true;
+      }
+    case "left":
+      if (
+        (snakeHead.y == myhead.y && snakeHead.x + 2 == myhead.x) ||
+        (snakeHead.y == myhead.y - 1 && snakeHead.x + 1 == myhead.x) ||
+        (snakeHead.y == myhead.y + 1 && snakeHead.x + 1 == myhead.x)
+      ) {
+        return true;
+      }
+  }
+  return false;
+}
+
+function headColisionDesision(
+  enemyHead: any[],
+  enemyLength: number,
+  myhead: any,
+  myLength: number,
+  possibleMoves: any
+): void {
+  for (let j = 0; j < possibleMoves.length; j++) {
+    if (possibleMoves[j]) {
+      if (isOnThisSide(enemyHead, myhead, possibleMoves)) {
+        if (enemyLength >= myLength) {
+          trace(`There a possible collision head on the ${possibleMoves[j]}`);
+          possibleMoves[j] = false;
+        }
+      }
+    }
+  }
+}
+
+const DEBUG = true;
+
+function trace(msg: any): void {
+  if (DEBUG) {
+    console.log(msg);
+  }
 }
 
 export function move(gameState: GameState): MoveResponse {
-    let possibleMoves: { [key: string]: boolean } = {
-        up: true,
-        down: true,
-        left: true,
-        right: true
+  let possibleMoves: { [key: string]: boolean } = {
+    up: true,
+    down: true,
+    left: true,
+    right: true,
+  };
+
+  trace(" === start Move ===");
+
+  // Step 0: Don't let your Battlesnake move back on it's own neck
+  const myHead = gameState.you.head;
+  const myNeck = gameState.you.body[1];
+  if (myNeck.x < myHead.x) {
+    trace("I am coming from left");
+    possibleMoves.left = false;
+  } else if (myNeck.x > myHead.x) {
+    trace("I am coming from right");
+    possibleMoves.right = false;
+  } else if (myNeck.y < myHead.y) {
+    trace("I am coming from down");
+    possibleMoves.down = false;
+  } else if (myNeck.y > myHead.y) {
+    trace("I am coming from up");
+    possibleMoves.up = false;
+  }
+
+  // TODO: Step 1 - Don't hit walls.
+  // Use information in gameState to prevent your Battlesnake from moving beyond the boundaries of the board.
+  if (myHead.x == gameState.board.width - 1) {
+    trace("There is a wall on the right");
+    possibleMoves.right = false;
+  }
+  if (myHead.x == 0) {
+    trace("There is a wall on the left");
+    possibleMoves.left = false;
+  }
+  if (myHead.y == 0) {
+    trace("There is a wall on the down");
+    possibleMoves.down = false;
+  }
+  if (myHead.y == gameState.board.height - 1) {
+    trace("There is a wall on the up");
+    possibleMoves.up = false;
+  }
+  trace(`Possible moves = ${JSON.stringify(possibleMoves)}`);
+
+  // TODO: Step 2 - Don't hit yourself.
+  // Use information in gameState to prevent your Battlesnake from colliding with itself.
+
+  trace(" ===== Step 2 - Don't hit yourself =====");
+  const mybody = gameState.you.body;
+  avoidSnakeBody(mybody, myHead, possibleMoves);
+  trace(`Possible moves = ${JSON.stringify(possibleMoves)}`);
+
+  // TODO: Step 3 - Don't collide with others.
+  // Use information in gameState to prevent your Battlesnake from colliding with others.
+  trace(" ===== Step 3 - Don't collide with others =====");
+  for (let i = 0; i < gameState.board.snakes.length; i++) {
+    avoidSnakeBody(gameState.board.snakes[i].body, myHead, possibleMoves);
+    headColisionDesision(
+      gameState.board.snakes[i].body,
+      gameState.board.snakes[i].length,
+      myHead,
+      gameState.you.length,
+      possibleMoves
+    );
+  }
+  trace(`Possible moves = ${JSON.stringify(possibleMoves)}`);
+
+  // TODO: Step 4 - Find food.
+  // Use information in gameState to seek out and find food.
+  trace(" ===== Step 4 - Find food =====");
+  for (let i = 0; i < gameState.board.food.length; i++) {
+    if (myHead.y == gameState.board.food[i].y) {
+      if (myHead.x == gameState.board.food[i].x - 1 && possibleMoves.right) {
+        trace("There is food on the right and it is safe");
+        possibleMoves.up = false;
+        possibleMoves.down = false;
+      } else if (
+        myHead.x == gameState.board.food[i].x + 1 &&
+        possibleMoves.left
+      ) {
+        trace("There is food on the left and it is safe");
+        possibleMoves.up = false;
+        possibleMoves.down = false;
+      }
+    } else if (myHead.x == gameState.board.food[i].x) {
+      if (myHead.y == gameState.board.food[i].y - 1 && possibleMoves.up) {
+        trace("There is food on the up and it is safe");
+        possibleMoves.left = false;
+        possibleMoves.right = false;
+      } else if (
+        myHead.y == gameState.board.food[i].y + 1 &&
+        possibleMoves.down
+      ) {
+        trace("There is food on the up and it down safe");
+        possibleMoves.left = false;
+        possibleMoves.right = false;
+      }
     }
+  }
 
-    // Step 0: Don't let your Battlesnake move back on it's own neck
-    const myHead = gameState.you.head
-    const myNeck = gameState.you.body[1]
-    if (myNeck.x < myHead.x) {
-        possibleMoves.left = false
-    } else if (myNeck.x > myHead.x) {
-        possibleMoves.right = false
-    } else if (myNeck.y < myHead.y) {
-        possibleMoves.down = false
-    } else if (myNeck.y > myHead.y) {
-        possibleMoves.up = false
-    }
 
-    // TODO: Step 1 - Don't hit walls.
-    // Use information in gameState to prevent your Battlesnake from moving beyond the boundaries of the board.
-    // const boardWidth = gameState.board.width
-    // const boardHeight = gameState.board.height
+  // Finally, choose a move from the available safe moves.
+  // TODO: Step 5 - Select a move to make based on strategy, rather than random.
+  trace(" ===== Step 5 - Choose a move =====");
+  trace(`Possible moves = ${JSON.stringify(possibleMoves)}`);
+  const safeMoves = Object.keys(possibleMoves).filter(
+    (key) => possibleMoves[key]
+  );
+  const response: MoveResponse = {
+    move: safeMoves[Math.floor(Math.random() * safeMoves.length)],
+  };
 
-    // TODO: Step 2 - Don't hit yourself.
-    // Use information in gameState to prevent your Battlesnake from colliding with itself.
-    // const mybody = gameState.you.body
-
-    // TODO: Step 3 - Don't collide with others.
-    // Use information in gameState to prevent your Battlesnake from colliding with others.
-
-    // TODO: Step 4 - Find food.
-    // Use information in gameState to seek out and find food.
-
-    // Finally, choose a move from the available safe moves.
-    // TODO: Step 5 - Select a move to make based on strategy, rather than random.
-    const safeMoves = Object.keys(possibleMoves).filter(key => possibleMoves[key])
-    const response: MoveResponse = {
-        move: safeMoves[Math.floor(Math.random() * safeMoves.length)],
-    }
-
-    console.log(`${gameState.game.id} MOVE ${gameState.turn}: ${response.move}`)
-    return response
+  console.log(`${gameState.game.id} MOVE ${gameState.turn}: ${response.move}`);
+  return response;
 }
